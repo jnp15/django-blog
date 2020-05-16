f06dcf8664a46c164dd32e72acd8321323b5aaf5
Jonathan Patten <jnp15@hotmail.com>
2020-05-16 12:33:43 -0700

    added logins

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,11 +2,14 @@
 
 from django.contrib import admin
 from django.urls import path, include  # <-- Make sure you have both of these imports.
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', include('blogging.urls')),  # <-- this didn't seem to work
     path('polling/', include('polling.urls')),  # <-- Add this
     path('blogging/', include('blogging.urls')),  # <-- I added this too
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
 
